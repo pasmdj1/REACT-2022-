8ad51c3a1010e548d34611d566af817442eb79d4
Pablo <pasmdj1@gmail.com>
2022-03-22 22:26:05 -0300

    es7

---
+++ src/components/Menu.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Menu = () => {
+
+    
+  return (
+    <div>
+    
+    <ul>
+        <li><a href="">pagina 1</a></li>
+        <li><a href="">pagina 2</a></li>
+        <li><a href="">pagina 3</a></li>
+    </ul>
+
+     
+    </div>
+   
+
+  )
+}
+
+export default Menu;
